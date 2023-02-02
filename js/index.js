5084ca6541f417505744c9332e7c7bb968c2a059
Angela Carvalho <angelalopo@outlook.com>
2023-02-02 09:29:49 -0300

    adiciona toFixed para formatar o resultado

--- js/index.js
+++ js/index.js
@@ -2,5 +2,6 @@ let valorEmDolar = 30;
 let cotacaoDoDolar = 5.35;
 
 let valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
 
-console.log(valorEmReal)
+console.log("R$", valorEmReal)
